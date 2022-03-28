bcf211b00bffa228e8c435568a549efd358f17ea
Ben Schmitt <bjschmitt@wpi.edu>
2022-03-27 21:15:04 -0400

    updated from base of spike b

---
+++ src/main/java/edu/wpi/cs3733/d22/teamY/Location.java
@@ -0,0 +1,45 @@
+package edu.wpi.cs3733.d22.teamY;
+
+public class Location {
+  String nodeID;
+  int xCoord, yCoord;
+  String floor;
+  String building;
+  String nodeType;
+  String longName;
+  String shortName;
+
+  public Location(
+      String nodeID,
+      int xCoord,
+      int yCoord,
+      String floor,
+      String building,
+      String nodeType,
+      String longName,
+      String shortName) {
+    this.nodeID = nodeID;
+    this.xCoord = xCoord;
+    this.yCoord = yCoord;
+    this.floor = floor;
+    this.building = building;
+    this.nodeType = nodeType;
+    this.longName = longName;
+    this.shortName = shortName;
+  }
+
+  public Location(String id) {
+    nodeID = id;
+    xCoord = 0;
+    yCoord = 0;
+    floor = "";
+    building = "";
+    nodeType = "";
+    longName = "";
+    shortName = "";
+  }
+
+  public Location getClone() {
+    return new Location(nodeID, xCoord, yCoord, floor, building, nodeType, longName, shortName);
+  }
+}
---
+++ src/main/java/edu/wpi/cs3733/d22/teamY/Main.java
@@ -0,0 +1,34 @@
+package edu.wpi.cs3733.d22.teamY;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    // creates database
+    Database locationDB = new Database("LocationDB");
+    Connection db_conn = locationDB.connection; // establishes connection to database
+
+    // creating dataManager class that manages database
+    LocationDataManager dataManager = new LocationDataManager(db_conn);
+    dataManager.cleanTable("locations");
+
+    // function set to read in CSV
+    ReadIn input = new ReadIn();
+    ArrayList<Location> locationArray = new ArrayList<Location>();
+    locationArray = input.readCSV();
+
+    // add ArrayList locations so that Datamanager can access all values
+    dataManager.addLocations(locationArray);
+
+    // creatae locationManager to run console loop
+    LocationManagerInterface locationManagerInterface = new LocationManagerInterface();
+    locationManagerInterface.start();
+    //    App.launch(App.class, args);
+
+    // once ended, shuts off the database
+    locationDB.shutdown_db();
+  }
+}
