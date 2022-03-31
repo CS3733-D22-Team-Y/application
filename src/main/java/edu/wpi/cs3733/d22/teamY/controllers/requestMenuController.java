09184df76b0f94a01fe55224de5d98337c17cedf
Nathan Anderson <nanderson2@wpi.edu>
2022-03-30 20:46:57 -0400

    Testing bugfix

--- src/main/java/edu/wpi/cs3733/d22/teamY/controllers/requestMenuController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/controllers/requestMenuController.java
@@ -1,8 +1,12 @@
 package edu.wpi.cs3733.d22.teamY.controllers;
 
+import edu.wpi.cs3733.d22.teamY.App;
 import java.awt.*;
 import java.io.IOException;
+import java.util.Objects;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 
 public class requestMenuController extends AbsGlobalControllerFuncs {
