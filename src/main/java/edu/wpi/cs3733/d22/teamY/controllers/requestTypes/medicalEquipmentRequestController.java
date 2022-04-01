f7d36c12eada4bae7cbaf92b0461d1c9877ee926
Nathan Anderson <nanderson2@wpi.edu>
2022-04-01 12:38:29 -0400

    Set up securityServicesRequestController to easily add a new request to the database.

--- src/main/java/edu/wpi/cs3733/d22/teamY/controllers/requestTypes/medicalEquipmentRequestController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/controllers/requestTypes/medicalEquipmentRequestController.java
@@ -7,7 +7,6 @@ import edu.wpi.cs3733.d22.teamY.MedEquipReq;
 import edu.wpi.cs3733.d22.teamY.model.dao.exception.DaoAddException;
 import java.io.IOException;
 import java.util.Objects;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
