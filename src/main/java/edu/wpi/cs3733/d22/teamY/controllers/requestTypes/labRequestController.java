6a16610d0fe10d56a8773d064fe1de4bf8e7f534
NathanAwpi <101899832+NathanAwpi@users.noreply.github.com>
2022-04-01 15:02:47 -0400

    Reformatted the request controllers (#13)
    Reformatted all request controllers for easier backend integration.
    Added RequestControllerUtil, which provides some general functions for all the service request controllers.
    All submit buttons will run the correct method when pressed
    
    Co-authored-by: emgore1016 <51887594+emgore1016@users.noreply.github.com>

--- src/main/java/edu/wpi/cs3733/d22/teamY/controllers/requestTypes/labRequestController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/controllers/requestTypes/labRequestController.java
@@ -1,6 +1,8 @@
 package edu.wpi.cs3733.d22.teamY.controllers.requestTypes;
 
 import com.jfoenix.controls.JFXRadioButton;
+import edu.wpi.cs3733.d22.teamY.App;
+import edu.wpi.cs3733.d22.teamY.model.dao.exception.DaoAddException;
 import edu.wpi.cs3733.d22.teamY.controllers.AbsGlobalControllerFuncs;
 import java.io.IOException;
 import javafx.event.ActionEvent;
@@ -22,6 +24,68 @@ public class labRequestController extends AbsGlobalControllerFuncs {
   @FXML private JFXRadioButton allergiesRadioButton;
   @FXML private JFXRadioButton diagnosisRadioButton;
 
+  // Result types text. These should be changed depending on what the names in the database are.
+  private final String antibodiesText = "antibodies";
+  private final String allergiesText = "allergies";
+  private final String diagnosisText = "diagnosis";
+
+  // BACKEND PEOPLE,THIS FUNCTION PASSES THE PARAMETERS TO THE DATABASE
+  /**
+   * Submits a service request.
+   *
+   * @param roomID The room ID.
+   * @param patientName The patient name.
+   * @param assignedNurse The assigned nurse.
+   * @param requestStatus The request status.
+   * @param additionalNotes Any additional notes.
+   * @param resultTypeSelected The type of result selected.
+   * @throws DaoAddException if there is an error adding something to the database (one of the
+   *     fields is invalid)
+   */
+  private void submitRequest(
+      String roomID,
+      String patientName,
+      String assignedNurse,
+      String requestStatus,
+      String additionalNotes,
+      String resultTypeSelected)
+      throws DaoAddException {
+    // Code to add the fields to the database goes here.
+  }
+
+  // Called when the submit button is pressed.
+  @FXML
+  void submitButton() {
+    // Checks if a lab result choice has been made.
+    if (RequestControllerUtil.isRadioButtonSelected(
+        allergiesRadioButton, antibodiesRadioButton, diagnosisRadioButton)) {
+      try {
+        submitRequest(
+            input_RoomID.getText(),
+            input_PatientName.getText(),
+            input_AssignedNurse.getText(),
+            input_RequestStatus.getText(),
+            input_AdditionalNotes.getText(),
+            getResultType());
+      }
+      // Thrown if one of the fields in submitRequest is invalid.
+      catch (DaoAddException e) {
+        System.out.println("One of more fields was invalid.");
+      }
+    } else {
+      System.out.println("Please select a result type.");
+    }
+  }
+
+  // Returns the database name of the selected radio button.
+  private String getResultType() {
+    if (antibodiesRadioButton.isSelected()) return antibodiesText;
+    if (allergiesRadioButton.isSelected()) return allergiesText;
+    if (diagnosisRadioButton.isSelected()) return diagnosisText;
+    // Will never be used
+    return "";
+  }
+
   @FXML
   void backToRequestMenu(ActionEvent event) throws IOException {
     loadScene("views/requestMenu.fxml");
@@ -31,13 +95,10 @@ public class labRequestController extends AbsGlobalControllerFuncs {
   // Reset button functionality
   @FXML
   void resetAllFields() {
-    antibodiesRadioButton.setSelected(false);
-    allergiesRadioButton.setSelected(false);
-    diagnosisRadioButton.setSelected(false);
-    input_RoomID.setText("");
-    input_PatientName.setText("");
-    input_AssignedNurse.setText("");
-    input_RequestStatus.setText("");
+    RequestControllerUtil.resetRadioButtons(
+        antibodiesRadioButton, allergiesRadioButton, diagnosisRadioButton);
+    RequestControllerUtil.resetTextFields(
+        input_RoomID, input_PatientName, input_AssignedNurse, input_RequestStatus);
     input_AdditionalNotes.setText("");
   }
 }
--- src/main/java/edu/wpi/cs3733/d22/teamY/controllers/requestTypes/laundryRequestController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/controllers/requestTypes/laundryRequestController.java
@@ -1,6 +1,8 @@
 package edu.wpi.cs3733.d22.teamY.controllers.requestTypes;
 
 import com.jfoenix.controls.JFXRadioButton;
+import edu.wpi.cs3733.d22.teamY.App;
+import edu.wpi.cs3733.d22.teamY.model.dao.exception.DaoAddException;
 import edu.wpi.cs3733.d22.teamY.controllers.AbsGlobalControllerFuncs;
 import java.io.IOException;
 import javafx.event.ActionEvent;
@@ -21,7 +23,70 @@ public class laundryRequestController extends AbsGlobalControllerFuncs {
   // Additional  Notes
   @FXML private TextArea input_AdditionalNotes;
 
+  private Scene requestMenu = null;
+
+  private final String hazardousText = "hazardous";
+  private final String scrubsText = "scrubs";
+  private final String linensText = "linens";
+
   public laundryRequestController() throws IOException {}
+
+  // BACKEND PEOPLE,THIS FUNCTION PASSES THE PARAMETERS TO THE DATABASE
+  /**
+   * Submits a service request.
+   *
+   * @param roomID The room ID.
+   * @param patientName The patient name.
+   * @param assignedNurse The assigned nurse.
+   * @param requestStatus The request status.
+   * @param additionalNotes Any additional notes.
+   * @param laundryTypeSelected The type of result selected.
+   * @throws DaoAddException if there is an error adding something to the database (one of the
+   *     fields is invalid)
+   */
+  private void submitRequest(
+      String roomID,
+      String patientName,
+      String assignedNurse,
+      String requestStatus,
+      String additionalNotes,
+      String laundryTypeSelected)
+      throws DaoAddException {
+    // Code to add the fields to the database goes here.
+  }
+
+  // Called when the submit button is pressed.
+  @FXML
+  void submitButton() {
+    // Checks if a lab result choice has been made.
+    if (RequestControllerUtil.isRadioButtonSelected(
+        hazardousRadioButton, linensRadioButton, scrubsRadioButton)) {
+      try {
+        submitRequest(
+            input_RoomID.getText(),
+            input_PatientName.getText(),
+            input_AssignedNurse.getText(),
+            input_RequestStatus.getText(),
+            input_AdditionalNotes.getText(),
+            getResultType());
+      }
+      // Thrown if one of the fields in submitRequest is invalid.
+      catch (DaoAddException e) {
+        System.out.println("One of more fields was invalid.");
+      }
+    } else {
+      System.out.println("Please select the type of laundry.");
+    }
+  }
+
+  // Returns the database name of the selected radio button.
+  private String getResultType() {
+    if (hazardousRadioButton.isSelected()) return hazardousText;
+    if (linensRadioButton.isSelected()) return linensText;
+    if (scrubsRadioButton.isSelected()) return scrubsText;
+    // Will never be used
+    return "";
+  }
 
   @FXML
   void backToRequestMenu(ActionEvent event) throws IOException {
@@ -31,13 +96,10 @@ public class laundryRequestController extends AbsGlobalControllerFuncs {
 
   @FXML
   void resetAllFields() {
-    hazardousRadioButton.setSelected(false);
-    scrubsRadioButton.setSelected(false);
-    linensRadioButton.setSelected(false);
-    input_RoomID.setText("");
-    input_PatientName.setText("");
-    input_AssignedNurse.setText("");
-    input_RequestStatus.setText("");
+    RequestControllerUtil.resetRadioButtons(
+        scrubsRadioButton, linensRadioButton, hazardousRadioButton);
+    RequestControllerUtil.resetTextFields(
+        input_RoomID, input_RequestStatus, input_AssignedNurse, input_PatientName);
     input_AdditionalNotes.setText("");
   }
 }
