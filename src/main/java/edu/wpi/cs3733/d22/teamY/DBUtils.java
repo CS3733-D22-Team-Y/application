ce0e195bf65132b72f92eef6837bcecc347aaaee
Nathan <ndpollock@wpi.edu>
2022-04-02 04:22:02 -0400

    testing and cleaning

--- src/main/java/edu/wpi/cs3733/d22/teamY/DBUtils.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/DBUtils.java
@@ -4,11 +4,13 @@ import edu.wpi.cs3733.d22.teamY.model.Location;
 import java.util.List;
 import org.hibernate.Session;
 
+/**
+ * This class is used to create specific queries for the database.
+ */
 public class DBUtils {
 
   private DBUtils() {}
 
-  // TODO test this
   @SuppressWarnings("unchecked")
   public static List<Location> getLocationsOnFloor(String floor) {
     Session s = SessionManager.getSession();
