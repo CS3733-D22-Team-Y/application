7bba9845d4e75db3522a1d21866509880ddb64f4
echo532 <84986609+echo532@users.noreply.github.com>
2022-04-03 11:08:39 -0400

    added function to check available Med Equipment

--- src/main/java/edu/wpi/cs3733/d22/teamY/DBUtils.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/DBUtils.java
@@ -1,12 +1,11 @@
 package edu.wpi.cs3733.d22.teamY;
 
 import edu.wpi.cs3733.d22.teamY.model.Location;
+import edu.wpi.cs3733.d22.teamY.model.MedEquip;
 import java.util.List;
 import org.hibernate.Session;
 
-/**
- * This class is used to create specific queries for the database.
- */
+/** This class is used to create specific queries for the database. */
 public class DBUtils {
 
   private DBUtils() {}
@@ -18,5 +17,26 @@ public class DBUtils {
         s.createQuery("from Location where floor = :floor").setParameter("floor", floor).list();
     s.close();
     return locations;
+  }
+
+  @SuppressWarnings("unchecked")
+  public static String getAvailableEquipment(String equipType) {
+    Session s = SessionManager.getSession();
+    List<MedEquip> equipment =
+        s.createQuery("from MedEquip where equipType = :equipType")
+            .setParameter("equipType", equipType)
+            .list();
+    s.close();
+
+    int total = equipment.size();
+    int available = 0;
+
+    for (MedEquip i : equipment) {
+      if (i.isClean().equals("1")) {
+        available++;
+      }
+    }
+
+    return (available + " of " + total);
   }
 }
