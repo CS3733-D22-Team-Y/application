c5d8336a55e34f0ffe73458b829fad67e0fd8dd5
echo532 <84986609+echo532@users.noreply.github.com>
2022-04-04 21:38:57 -0400

    Update DBUtils.java

--- src/main/java/edu/wpi/cs3733/d22/teamY/DBUtils.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/DBUtils.java
@@ -2,9 +2,9 @@ package edu.wpi.cs3733.d22.teamY;
 
 import edu.wpi.cs3733.d22.teamY.model.Location;
 import edu.wpi.cs3733.d22.teamY.model.MedEquip;
-import javafx.util.Pair;
-
 import java.util.List;
+import javafx.util.Pair;
+import org.hibernate.Session;
 
 /** This class is used to create specific queries for the database. */
 public class DBUtils {
@@ -15,7 +15,7 @@ public class DBUtils {
     return DBManager.getAll(Location.class, new Where(Location.FLOOR, floor));
   }
 
-  public static Pair<Integer,Integer> getAvailableEquipment(String equipType) {
+  public static Pair<Integer, Integer> getAvailableEquipment(String equipType) {
     List<MedEquip> equipment =
         DBManager.getAll(MedEquip.class, new Where(MedEquip.EQUIP_TYPE, equipType));
 
@@ -33,5 +33,26 @@ public class DBUtils {
     }
     Pair<Integer, Integer> returnPair = new Pair<>(available, total);
     return returnPair;
+  }
+
+  // find MedEquip object
+  public static void updateCleanStatus(String equipType, String locationID) {
+    Session s = SessionManager.getSession();
+    List<MedEquip> equipment =
+        s.createQuery("from MedEquip where equipType = :equipType and isClean = '1'")
+            .setParameter("equipType", equipType)
+            .list();
+    s.close();
+
+    if (equipment.size() == 0) {
+      return;
+    }
+
+    MedEquip thisEquip = equipment.get(0);
+
+    thisEquip.setEquipLocId(locationID);
+    thisEquip.setClean("0");
+
+    DBManager.update(thisEquip);
   }
 }
