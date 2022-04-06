6d13e41a5640892e767b64530f7610c14742c509
Ben Schmitt <bjschmitt@wpi.edu>
2022-04-05 23:06:48 -0400

    removed print

--- src/main/java/edu/wpi/cs3733/d22/teamY/Main.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/Main.java
@@ -9,8 +9,6 @@ public class Main {
       CSVBackup.loadFromCSV(e);
     }
 
-    System.out.println(DBUtils.getNameFromID("00043"));
-
     App.launch(App.class, args);
 
     // Backup all tables to CSV on shutdown
