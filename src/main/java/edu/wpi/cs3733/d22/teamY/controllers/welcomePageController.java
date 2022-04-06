a55cfe0bdb2daf177d4db115bb8beefb869f9ef3
emgore1016 <51887594+emgore1016@users.noreply.github.com>
2022-04-05 20:25:25 -0400

    Egorelik/kill from welcome (#39)
    * change button order, remove ground floor
    
    * made welcome page's exit button kill the application rather than prompt the popup

--- src/main/java/edu/wpi/cs3733/d22/teamY/controllers/welcomePageController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/controllers/welcomePageController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.animation.FadeTransition;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -32,8 +33,7 @@ public class welcomePageController {
 
   @FXML
   void killApplication() throws IOException {
-    System.out.println("pressed button");
-    SceneLoading.loadPopup("views/ConfirmClose.fxml", "views/welcomePage.fxml");
+    Platform.exit();
   }
 
   @FXML
