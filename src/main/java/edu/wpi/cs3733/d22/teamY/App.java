4236d2659a378ad6a9f2e8c674fff6f90fd4cd4e
emgore1016 <51887594+emgore1016@users.noreply.github.com>
2022-04-09 00:13:59 -0400

    comment out camera code (FU connor and nathan p)

--- src/main/java/edu/wpi/cs3733/d22/teamY/App.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/App.java
@@ -39,7 +39,7 @@ public class App extends Application {
     Scene scene = new Scene(root);
     primaryStage.setScene(scene);
     primaryStage.show();
-    camera.newPfp();
+    // camera.newPfp();
   }
 
   @Override
