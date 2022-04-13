acc63dfc003e87570db5cd3e6dd62dceffde1ebf
emgore1016 <51887594+emgore1016@users.noreply.github.com>
2022-04-12 21:17:20 -0400

    adslk;gnvas

--- src/main/java/edu/wpi/cs3733/d22/teamY/controllers/PersonalSettingsController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/controllers/PersonalSettingsController.java
@@ -1,20 +1,22 @@
 package edu.wpi.cs3733.d22.teamY.controllers;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXHamburger;
+import edu.wpi.cs3733.d22.teamY.DBManager;
 import edu.wpi.cs3733.d22.teamY.model.Employee;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 
 public class PersonalSettingsController {
-  // Base pane for displaying new scenes
-  @FXML private Pane mapPane;
-  // Sidebar
-  @FXML private Pane sidebarPane;
-  @FXML private JFXButton closeSidebarHiddenButton;
-  @FXML private JFXHamburger sidebarHamburger;
+
+  @FXML MFXTextField legalName;
+  @FXML MFXTextField prefName;
+  @FXML MFXTextField title;
+  @FXML MFXTextField email;
+  @FXML MFXTextField dob;
+  @FXML MFXTextField pronouns;
+  @FXML MFXTextField phone;
+
   AnchorPane sidebar = null;
   public static Employee currentEmployee =
       new Employee(
@@ -22,10 +24,66 @@ public class PersonalSettingsController {
           " ", " ");
 
   @FXML
-  void initialize() throws IOException {}
+  void initialize() throws IOException {
+    legalName.setText(PersonalSettings.currentEmployee.getName());
+    prefName.setText(PersonalSettings.currentEmployee.getPrefName());
+    title.setText(PersonalSettings.currentEmployee.getRole());
+    email.setText(PersonalSettings.currentEmployee.getEmail());
+    dob.setText(PersonalSettings.currentEmployee.getDOB());
+    pronouns.setText(PersonalSettings.currentEmployee.getPronouns());
+    phone.setText(PersonalSettings.currentEmployee.getPhone());
+  }
 
   @FXML
-  void mainMenu() throws IOException {
-    SceneLoading.loadScene("views/Map.fxml");
+  public void updateLegalName() {
+    PersonalSettings.currentEmployee.setName(legalName.getText());
+    DBManager.update(PersonalSettings.currentEmployee);
+  }
+
+  @FXML
+  public void updatePrefName() {
+    PersonalSettings.currentEmployee.setPrefName(prefName.getText());
+    DBManager.update(PersonalSettings.currentEmployee);
+  }
+
+  @FXML
+  public void updateTitle() {
+    PersonalSettings.currentEmployee.setRole(title.getText());
+    DBManager.update(PersonalSettings.currentEmployee);
+  }
+
+  @FXML
+  public void updateEmail() {
+    PersonalSettings.currentEmployee.setEmail(email.getText());
+    DBManager.update(PersonalSettings.currentEmployee);
+  }
+
+  @FXML
+  public void updateDob() {
+    PersonalSettings.currentEmployee.setDOB(dob.getText());
+    DBManager.update(PersonalSettings.currentEmployee);
+  }
+
+  @FXML
+  public void updatePronouns() {
+    PersonalSettings.currentEmployee.setPronouns(pronouns.getText());
+    DBManager.update(PersonalSettings.currentEmployee);
+  }
+
+  @FXML
+  public void updatePhone() {
+    PersonalSettings.currentEmployee.setPhone(phone.getText());
+    DBManager.update(PersonalSettings.currentEmployee);
+  }
+
+  @FXML
+  public void updateFields() {
+    updateLegalName();
+    updatePrefName();
+    updateTitle();
+    updateEmail();
+    updateDob();
+    updatePronouns();
+    updatePhone();
   }
 }
