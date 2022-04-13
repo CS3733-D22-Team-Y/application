dab9c4b0166e931c26bc0e5182db49ace50c3474
NathanAwpi <nanderson2@wpi.edu>
2022-04-13 14:20:00 -0400

    map ui changes

--- src/main/java/edu/wpi/cs3733/d22/teamY/controllers/MapPageController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/controllers/MapPageController.java
@@ -783,7 +783,9 @@ public class MapPageController<T extends Requestable> {
 
     // Load initial floor and mode
     switchMap(lastFloor, MapMode.LOCATION);
-    modeBox.setValue("");
+    equipInfoPane.setVisible(false);
+    reqInfoPane.setVisible(false);
+    locationInfoPane.setVisible(false);
   }
 
   public void exit() {
