1c79a5b39b1fb0db940af6c30ba863a33ca8121f
emgore1016 <51887594+emgore1016@users.noreply.github.com>
2022-04-17 21:30:35 -0400

    create languages list

---
+++ src/main/java/edu/wpi/cs3733/d22/teamY/utilTemp/Languages.java
@@ -0,0 +1,41 @@
+package edu.wpi.cs3733.d22.teamY.utilTemp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Languages {
+    static List langs = new ArrayList();
+    public static void Languages(){
+        langs.add("English");
+        langs.add("Mandarin Chinese");
+        langs.add("Hindi");
+        langs.add("Spanish");
+        langs.add("French");
+        langs.add("Arabic");
+        langs.add("Bengali");
+        langs.add("Russian");
+        langs.add("Portuguese");
+        langs.add("Urdu");
+        langs.add("Indonesian");
+        langs.add("German");
+        langs.add("Japanese");
+        langs.add("Nigerian Pidgin");
+        langs.add("Marathi");
+        langs.add("Telugu");
+        langs.add("Turkish");
+        langs.add("Tamil");
+        langs.add("Yue Chinese");
+        langs.add("Vietnamese");
+    }
+    public List getLanguages(){
+        return langs;
+    }
+
+    public void addLanguage(String lang){
+        langs.add(lang);
+    }
+
+    public boolean isLanguage(String check){
+        return langs.contains(check);
+    }
+}
