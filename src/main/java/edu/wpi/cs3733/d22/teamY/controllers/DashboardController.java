93a98dda2c1489bc24edae6d7630014015077e8a
DataPointBeing <jcunown@live.com>
2022-04-19 20:56:34 -0400

    finished weather functionality

--- src/main/java/edu/wpi/cs3733/d22/teamY/controllers/DashboardController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/controllers/DashboardController.java
@@ -1,76 +1,152 @@
 package edu.wpi.cs3733.d22.teamY.controllers;
 
-import com.jfoenix.controls.JFXComboBox;
-import edu.wpi.cs3733.d22.teamY.DBManager;
-import edu.wpi.cs3733.d22.teamY.DBUtils;
-import edu.wpi.cs3733.d22.teamY.EntryType;
-import edu.wpi.cs3733.d22.teamY.model.LaundryRequest;
-import edu.wpi.cs3733.d22.teamY.model.RequestStatus;
-import io.github.palexdev.materialfx.controls.MFXRadioButton;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import edu.wpi.cs3733.d22.teamY.App;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class DashboardController {
-  // Radio Buttons
-  @FXML private MFXRadioButton hazardousRadioButton;
-  @FXML private MFXRadioButton scrubsRadioButton;
-  @FXML private MFXRadioButton linensRadioButton;
-  // Text inputs
-  @FXML private TextField input_AssignedNurse;
-  @FXML private JFXComboBox<String> roomsComboBox;
-  @FXML private TextField roomsHiddenField;
-  // Additional  Notes
-  @FXML private TextArea input_AdditionalNotes;
-  // Error Label
-  @FXML private TextArea errorLabel;
-  // Side bar
-  @FXML private AnchorPane sidebarPane;
 
-  private Scene requestMenu = null;
+  private static final String key = "2cad081d3a66daa8ee82b4abeaabb308";
 
-  private final String hazardousText = "hazardous";
-  private final String scrubsText = "scrubs";
-  private final String linensText = "linens";
+  @FXML private Label timeDisplay;
+  @FXML private Label dateDisplay;
 
-  public DashboardController() {}
+  @FXML private Label fahrenheit;
+  @FXML private Label celsius;
 
-  public void initialize() throws IOException {}
+  @FXML private ImageView weatherIcon;
 
-  // BACKEND PEOPLE,THIS FUNCTION PASSES THE PARAMETERS TO THE DATABASE
+  public void initialize() throws IOException {
+    AnimationTimer timer =
+        new AnimationTimer() {
+          private long lastUpdate = 0;
 
-  /**
-   * Submits a service request.
-   *
-   * @param roomID The room ID.
-   * @param additionalNotes Any additional notes.
-   * @param laundryTypeSelected The type of result selected.
-   */
-  private void submitRequest(
-      String roomID, String assignedNurse, String additionalNotes, String laundryTypeSelected) {
-    String nextRequest = String.valueOf(DBUtils.getNextRequestNum(EntryType.LAUNDRY_REQUEST));
-    DBManager.save(
-        new LaundryRequest(
-            nextRequest,
-            roomID,
-            assignedNurse,
-            RequestStatus.INCOMPLETE,
-            additionalNotes,
-            laundryTypeSelected));
-    System.out.println("Saved Laundry Request");
+          @Override
+          public void handle(long now) {
+            timeDisplay.setText(
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("h:mma")).toLowerCase());
+            dateDisplay.setText(
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("EEEE, LLL d")));
+            if (now - lastUpdate >= 60000000000L) {
+              updateWeather();
+              lastUpdate = now;
+            }
+          }
+        };
+    updateWeather();
+    timer.start();
   }
 
-  // Called when the submit button is pressed.
+  private void updateWeather() {
+    try {
+      URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=" + key);
+      HttpURLConnection con = (HttpURLConnection) url.openConnection();
+      con.setRequestMethod("GET");
+      con.connect();
 
-  // Returns the database name of the selected radio button.
-  private String getResultType() {
-    if (hazardousRadioButton.isSelected()) return hazardousText;
-    if (linensRadioButton.isSelected()) return linensText;
-    if (scrubsRadioButton.isSelected()) return scrubsText;
-    // Will never be used
-    return "";
+      BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+      String inputLine;
+      StringBuilder resp = new StringBuilder();
+      while ((inputLine = in.readLine()) != null) {
+        resp.append(inputLine);
+      }
+      in.close();
+
+      JsonObject json = JsonParser.parseString(resp.toString()).getAsJsonObject();
+
+      int weatherID =
+          json.get("weather").getAsJsonArray().get(0).getAsJsonObject().get("id").getAsInt();
+      float temperature = json.get("main").getAsJsonObject().get("temp").getAsFloat();
+      long sRise = json.get("sys").getAsJsonObject().get("sunrise").getAsLong();
+      long sSet = json.get("sys").getAsJsonObject().get("sunset").getAsLong();
+
+      long tFah = Math.round((((temperature - 273.15) * 9) / 5) + 32);
+      long tCel = Math.round(temperature - 273.15);
+
+      fahrenheit.setText(String.valueOf(tFah));
+      celsius.setText(String.valueOf(tCel));
+
+      LocalDateTime ldt = LocalDateTime.ofInstant(new Date().toInstant(), ZoneId.systemDefault());
+
+      Date now = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+      Date sunRise = new java.util.Date(sRise * 1000);
+      Date sunSet = new java.util.Date(sSet * 1000);
+
+      boolean isDay = now.after(sunRise) && now.before(sunSet);
+
+      weatherIcon.setImage(
+          new Image(String.valueOf(App.class.getResource(weatherImagePath(weatherID, isDay)))));
+
+      // System.out.println("Weather updated successfully!!!");
+    } catch (Exception ignored) {
+      ignored.printStackTrace();
+    }
+  }
+
+  private String weatherImagePath(int id, boolean daytime) {
+    String path = "views/images/icons/dash/weather/";
+    String im = "unknown";
+    String timeStr = daytime ? "_day" : "_night";
+    switch (id / 100) {
+      case 2:
+        im = "thunder";
+        break;
+      case 3:
+        im = "rain";
+        break;
+      case 5:
+        switch (id / 10) {
+          case 50:
+            im = ("rain" + timeStr);
+            break;
+          case 51:
+            im = "snow";
+            break;
+          case 52:
+          case 53:
+            im = "rain";
+            break;
+        }
+        break;
+      case 6:
+        im = "snow";
+        break;
+      case 7:
+        im = "fog";
+        break;
+      case 8:
+        switch (id) {
+          case 800:
+            im = ("clear" + timeStr);
+            break;
+          case 801:
+            im = ("partlycloudy" + timeStr);
+            break;
+          case 802:
+            im = "cloudy";
+            break;
+          case 803:
+          case 804:
+            im = "stormclouds";
+            break;
+        }
+        break;
+    }
+
+    return path + im + ".png";
   }
 }
