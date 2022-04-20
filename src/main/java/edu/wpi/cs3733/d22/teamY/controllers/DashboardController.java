059e5893e0c85ee97b5d9c58a0ee2c0735d6d68b
DataPointBeing <jcunown@live.com>
2022-04-19 22:58:19 -0400

    TABS

--- src/main/java/edu/wpi/cs3733/d22/teamY/controllers/DashboardController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/controllers/DashboardController.java
@@ -32,294 +32,299 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 
 public class DashboardController {
-  // Radio Buttons
-  public static LinkedList<String> alerts = new LinkedList<>();
-  // Text inputs
 
-  // Side bar
-  private static final String key = "2cad081d3a66daa8ee82b4abeaabb308";
+	static class EquipmentMonitor {
+		
+	}
 
-  @FXML private Label timeDisplay;
-  @FXML private Label dateDisplay;
+	// Radio Buttons
+	public static LinkedList<String> alerts = new LinkedList<>();
+	// Text inputs
 
-  @FXML private Label fahrenheit;
-  @FXML private Label celsius;
+	// Side bar
+	private static final String key = "2cad081d3a66daa8ee82b4abeaabb308";
 
-  @FXML private ImageView weatherIcon;
+	@FXML private Label timeDisplay;
+	@FXML private Label dateDisplay;
 
-  @FXML private AnchorPane sidebarPane;
+	@FXML private Label fahrenheit;
+	@FXML private Label celsius;
 
-  @FXML private HBox alertsBox;
+	@FXML private ImageView weatherIcon;
 
-  @FXML private TextArea testAlert;
+	@FXML private AnchorPane sidebarPane;
 
-  private Scene requestMenu = null;
+	@FXML private HBox alertsBox;
 
-  public DashboardController() {}
+	@FXML private TextArea testAlert;
 
-  public void initialize() throws IOException {
+	private Scene requestMenu = null;
 
-    AnimationTimer timer =
-            new AnimationTimer() {
-              private long lastUpdate = 0;
+	public DashboardController() {}
 
-              @Override
-              public void handle(long now) {
-                timeDisplay.setText(
-                        LocalDateTime.now().format(DateTimeFormatter.ofPattern("h:mma")).toLowerCase());
-                dateDisplay.setText(
-                        LocalDateTime.now().format(DateTimeFormatter.ofPattern("EEEE, LLL d")));
-                if (now - lastUpdate >= 60000000000L) {
-                  updateWeather();
-                  lastUpdate = now;
-                }
-              }
-            };
-    updateWeather();
-    timer.start();
+	public void initialize() throws IOException {
 
-    // requestBox.setPrefHeight(1000);
-    alertsBox.setBackground(Background.EMPTY);
+		AnimationTimer timer =
+				new AnimationTimer() {
+					private long lastUpdate = 0;
 
-    if (DBUtils.checkAvailableEquipmentOnFloor("3", "PUMP") < 5) {
+					@Override
+					public void handle(long now) {
+						timeDisplay.setText(
+								LocalDateTime.now().format(DateTimeFormatter.ofPattern("h:mma")).toLowerCase());
+						dateDisplay.setText(
+								LocalDateTime.now().format(DateTimeFormatter.ofPattern("EEEE, LLL d")));
+						if (now - lastUpdate >= 60000000000L) {
+							updateWeather();
+							lastUpdate = now;
+						}
+					}
+				};
+		updateWeather();
+		timer.start();
 
-      addToBox("Floor 3 has less than 5 Infusion Pumps");
-      new ServiceRequest(
-          RequestTypes.MEDEQUIP,
-          "",
-          "",
-          "supply an infusion pump to floor 3",
-          1,
-          RequestStatus.INCOMPLETE,
-          new String[] {"PUMP"});
-    }
-    if (DBUtils.checkAvailableEquipmentOnFloor("4", "PUMP") < 5) {
+		// requestBox.setPrefHeight(1000);
+		alertsBox.setBackground(Background.EMPTY);
 
-      addToBox("Floor 4 has less than 5 Infusion Pumps");
-      new ServiceRequest(
-          RequestTypes.MEDEQUIP,
-          "",
-          "",
-          "supply an infusion pump to floor 4",
-          1,
-          RequestStatus.INCOMPLETE,
-          new String[] {"PUMP"});
-    }
-    if (DBUtils.checkAvailableEquipmentOnFloor("5", "PUMP") < 5) {
+		if (DBUtils.checkAvailableEquipmentOnFloor("3", "PUMP") < 5) {
 
-      addToBox("Floor 5 has less than 5 Infusion Pumps");
-      new ServiceRequest(
-          RequestTypes.MEDEQUIP,
-          "",
-          "",
-          "supply an infusion pump to floor 5",
-          1,
-          RequestStatus.INCOMPLETE,
-          new String[] {"PUMP"});
-    }
+			addToBox("Floor 3 has less than 5 Infusion Pumps");
+			new ServiceRequest(
+					RequestTypes.MEDEQUIP,
+					"",
+					"",
+					"supply an infusion pump to floor 3",
+					1,
+					RequestStatus.INCOMPLETE,
+					new String[] {"PUMP"});
+		}
+		if (DBUtils.checkAvailableEquipmentOnFloor("4", "PUMP") < 5) {
 
-    if (DBUtils.checkUnavailableEquipmentOnFloor("3", "PUMP") >= 10) {
+			addToBox("Floor 4 has less than 5 Infusion Pumps");
+			new ServiceRequest(
+					RequestTypes.MEDEQUIP,
+					"",
+					"",
+					"supply an infusion pump to floor 4",
+					1,
+					RequestStatus.INCOMPLETE,
+					new String[] {"PUMP"});
+		}
+		if (DBUtils.checkAvailableEquipmentOnFloor("5", "PUMP") < 5) {
 
-      addToBox("Floor 3 has 10 dirty Infusion Pumps");
-      new ServiceRequest(
-          RequestTypes.MEDEQUIP,
-          "",
-          "",
-          "clean an infusion pump on floor 3",
-          1,
-          RequestStatus.INCOMPLETE,
-          new String[] {"PUMP"});
-    }
-    if (DBUtils.checkUnavailableEquipmentOnFloor("4", "PUMP") >= 10) {
+			addToBox("Floor 5 has less than 5 Infusion Pumps");
+			new ServiceRequest(
+					RequestTypes.MEDEQUIP,
+					"",
+					"",
+					"supply an infusion pump to floor 5",
+					1,
+					RequestStatus.INCOMPLETE,
+					new String[] {"PUMP"});
+		}
 
-      addToBox("Floor 4 has 10 dirty Infusion Pumps");
-      new ServiceRequest(
-          RequestTypes.MEDEQUIP,
-          "",
-          "",
-          "clean an infusion pump on floor 4",
-          1,
-          RequestStatus.INCOMPLETE,
-          new String[] {"PUMP"});
-    }
-    if (DBUtils.checkUnavailableEquipmentOnFloor("5", "PUMP") >= 10) {
+		if (DBUtils.checkUnavailableEquipmentOnFloor("3", "PUMP") >= 10) {
 
-      addToBox("Floor 5 has 10 dirty Infusion Pumps");
-      new ServiceRequest(
-          RequestTypes.MEDEQUIP,
-          "",
-          "",
-          "clean an infusion pump on floor 5",
-          1,
-          RequestStatus.INCOMPLETE,
-          new String[] {"PUMP"});
-    }
+			addToBox("Floor 3 has 10 dirty Infusion Pumps");
+			new ServiceRequest(
+					RequestTypes.MEDEQUIP,
+					"",
+					"",
+					"clean an infusion pump on floor 3",
+					1,
+					RequestStatus.INCOMPLETE,
+					new String[] {"PUMP"});
+		}
+		if (DBUtils.checkUnavailableEquipmentOnFloor("4", "PUMP") >= 10) {
 
-    if (DBUtils.checkUnavailableEquipmentOnFloor("3", "BED") >= 6) {
+			addToBox("Floor 4 has 10 dirty Infusion Pumps");
+			new ServiceRequest(
+					RequestTypes.MEDEQUIP,
+					"",
+					"",
+					"clean an infusion pump on floor 4",
+					1,
+					RequestStatus.INCOMPLETE,
+					new String[] {"PUMP"});
+		}
+		if (DBUtils.checkUnavailableEquipmentOnFloor("5", "PUMP") >= 10) {
 
-      addToBox("Floor 3 has 6 dirty beds");
-      new ServiceRequest(
-          RequestTypes.MEDEQUIP,
-          "",
-          "",
-          "clean a bed on floor 3",
-          1,
-          RequestStatus.INCOMPLETE,
-          new String[] {"PUMP"});
-    }
-    if (DBUtils.checkUnavailableEquipmentOnFloor("4", "BED") >= 6) {
+			addToBox("Floor 5 has 10 dirty Infusion Pumps");
+			new ServiceRequest(
+					RequestTypes.MEDEQUIP,
+					"",
+					"",
+					"clean an infusion pump on floor 5",
+					1,
+					RequestStatus.INCOMPLETE,
+					new String[] {"PUMP"});
+		}
 
-      addToBox("Floor 4 has 6 dirty beds");
-      new ServiceRequest(
-          RequestTypes.MEDEQUIP,
-          "",
-          "",
-          "clean a bed on floor 4",
-          1,
-          RequestStatus.INCOMPLETE,
-          new String[] {"PUMP"});
-    }
-    if (DBUtils.checkUnavailableEquipmentOnFloor("5", "BED") >= 6) {
+		if (DBUtils.checkUnavailableEquipmentOnFloor("3", "BED") >= 6) {
 
-      addToBox("Floor 5 has 6 dirty beds");
-      new ServiceRequest(
-          RequestTypes.MEDEQUIP,
-          "",
-          "",
-          "clean a bed on floor 5",
-          1,
-          RequestStatus.INCOMPLETE,
-          new String[] {"PUMP"});
-    }
+			addToBox("Floor 3 has 6 dirty beds");
+			new ServiceRequest(
+					RequestTypes.MEDEQUIP,
+					"",
+					"",
+					"clean a bed on floor 3",
+					1,
+					RequestStatus.INCOMPLETE,
+					new String[] {"PUMP"});
+		}
+		if (DBUtils.checkUnavailableEquipmentOnFloor("4", "BED") >= 6) {
 
-    NewSceneLoading.loadSidebar(sidebarPane);
-  }
+			addToBox("Floor 4 has 6 dirty beds");
+			new ServiceRequest(
+					RequestTypes.MEDEQUIP,
+					"",
+					"",
+					"clean a bed on floor 4",
+					1,
+					RequestStatus.INCOMPLETE,
+					new String[] {"PUMP"});
+		}
+		if (DBUtils.checkUnavailableEquipmentOnFloor("5", "BED") >= 6) {
 
-  private void updateWeather() {
-    try {
-      URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=" + key);
-      HttpURLConnection con = (HttpURLConnection) url.openConnection();
-      con.setRequestMethod("GET");
-      con.connect();
+			addToBox("Floor 5 has 6 dirty beds");
+			new ServiceRequest(
+					RequestTypes.MEDEQUIP,
+					"",
+					"",
+					"clean a bed on floor 5",
+					1,
+					RequestStatus.INCOMPLETE,
+					new String[] {"PUMP"});
+		}
 
-      BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-      String inputLine;
-      StringBuilder resp = new StringBuilder();
-      while ((inputLine = in.readLine()) != null) {
-        resp.append(inputLine);
-      }
-      in.close();
+		NewSceneLoading.loadSidebar(sidebarPane);
+	}
 
-      JsonObject json = JsonParser.parseString(resp.toString()).getAsJsonObject();
+	private void updateWeather() {
+		try {
+			URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=" + key);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			con.connect();
 
-      int weatherID =
-              json.get("weather").getAsJsonArray().get(0).getAsJsonObject().get("id").getAsInt();
-      float temperature = json.get("main").getAsJsonObject().get("temp").getAsFloat();
-      long sRise = json.get("sys").getAsJsonObject().get("sunrise").getAsLong();
-      long sSet = json.get("sys").getAsJsonObject().get("sunset").getAsLong();
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuilder resp = new StringBuilder();
+			while ((inputLine = in.readLine()) != null) {
+				resp.append(inputLine);
+			}
+			in.close();
 
-      long tFah = Math.round((((temperature - 273.15) * 9) / 5) + 32);
-      long tCel = Math.round(temperature - 273.15);
+			JsonObject json = JsonParser.parseString(resp.toString()).getAsJsonObject();
 
-      fahrenheit.setText(String.valueOf(tFah));
-      celsius.setText(String.valueOf(tCel));
+			int weatherID =
+					json.get("weather").getAsJsonArray().get(0).getAsJsonObject().get("id").getAsInt();
+			float temperature = json.get("main").getAsJsonObject().get("temp").getAsFloat();
+			long sRise = json.get("sys").getAsJsonObject().get("sunrise").getAsLong();
+			long sSet = json.get("sys").getAsJsonObject().get("sunset").getAsLong();
 
-      LocalDateTime ldt = LocalDateTime.ofInstant(new Date().toInstant(), ZoneId.systemDefault());
+			long tFah = Math.round((((temperature - 273.15) * 9) / 5) + 32);
+			long tCel = Math.round(temperature - 273.15);
 
-      Date now = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
-      Date sunRise = new java.util.Date(sRise * 1000);
-      Date sunSet = new java.util.Date(sSet * 1000);
+			fahrenheit.setText(String.valueOf(tFah));
+			celsius.setText(String.valueOf(tCel));
 
-      boolean isDay = now.after(sunRise) && now.before(sunSet);
+			LocalDateTime ldt = LocalDateTime.ofInstant(new Date().toInstant(), ZoneId.systemDefault());
 
-      weatherIcon.setImage(
-              new Image(String.valueOf(App.class.getResource(weatherImagePath(weatherID, isDay)))));
+			Date now = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+			Date sunRise = new java.util.Date(sRise * 1000);
+			Date sunSet = new java.util.Date(sSet * 1000);
 
-      // System.out.println("Weather updated successfully!!!");
-    } catch (Exception ignored) {
-      //ignored.printStackTrace();
-    }
-  }
+			boolean isDay = now.after(sunRise) && now.before(sunSet);
 
-  public static void reloadDashboard() {}
+			weatherIcon.setImage(
+					new Image(String.valueOf(App.class.getResource(weatherImagePath(weatherID, isDay)))));
 
-  private void addRequest(Requestable req) throws IOException {
+			// System.out.println("Weather updated successfully!!!");
+		} catch (Exception ignored) {
+			//ignored.printStackTrace();
+		}
+	}
 
-    // System.out.println(ActiveServiceRequestController.requestControllers.getLast());
+	public static void reloadDashboard() {}
 
-    // rqPairs.add(new RequestSet(controller, pane, req));
-  }
+	private void addRequest(Requestable req) throws IOException {
 
-  private void addToBox(String inputText) {
+		// System.out.println(ActiveServiceRequestController.requestControllers.getLast());
 
-    // Label test = new Label("test");
-    // test.setBackground(new Background(new BackgroundFill(Paint.TRANSLUCENT, CornerRadii.EMPTY,
-    // Insets.EMPTY)));
-    Label testLabel = new Label(inputText);
-    testLabel.setStyle("-fx-font-size: 20");
-    testLabel.setStyle("-fx-border-color: black;");
-    testLabel.setMinWidth(262);
+		// rqPairs.add(new RequestSet(controller, pane, req));
+	}
 
-    alertsBox.getChildren().add(testLabel);
+	private void addToBox(String inputText) {
 
-    // rqSet.getController().populateFromRequestable(rqSet.getRequest());
+		// Label test = new Label("test");
+		// test.setBackground(new Background(new BackgroundFill(Paint.TRANSLUCENT, CornerRadii.EMPTY,
+		// Insets.EMPTY)));
+		Label testLabel = new Label(inputText);
+		testLabel.setStyle("-fx-font-size: 20");
+		testLabel.setStyle("-fx-border-color: black;");
+		testLabel.setMinWidth(262);
+
+		alertsBox.getChildren().add(testLabel);
+
+		// rqSet.getController().populateFromRequestable(rqSet.getRequest());
 
 
 
 
 
 
-  }
+	}
 
-  private String weatherImagePath(int id, boolean daytime) {
-    String path = "views/images/icons/dash/weather/";
-    String im = "unknown";
-    String timeStr = daytime ? "_day" : "_night";
-    switch (id / 100) {
-      case 2:
-        im = "thunder";
-        break;
-      case 3:
-        im = "rain";
-        break;
-      case 5:
-        switch (id / 10) {
-          case 50:
-            im = ("rain" + timeStr);
-            break;
-          case 51:
-            im = "snow";
-            break;
-          case 52:
-          case 53:
-            im = "rain";
-            break;
-        }
-        break;
-      case 6:
-        im = "snow";
-        break;
-      case 7:
-        im = "fog";
-        break;
-      case 8:
-        switch (id) {
-          case 800:
-            im = ("clear" + timeStr);
-            break;
-          case 801:
-            im = ("partlycloudy" + timeStr);
-            break;
-          case 802:
-            im = "cloudy";
-            break;
-          case 803:
-          case 804:
-            im = "stormclouds";
-            break;
-        }
-        break;
-    }
-    return path + im + ".png";
-  }
+	private String weatherImagePath(int id, boolean daytime) {
+		String path = "views/images/icons/dash/weather/";
+		String im = "unknown";
+		String timeStr = daytime ? "_day" : "_night";
+		switch (id / 100) {
+			case 2:
+				im = "thunder";
+				break;
+			case 3:
+				im = "rain";
+				break;
+			case 5:
+				switch (id / 10) {
+					case 50:
+						im = ("rain" + timeStr);
+						break;
+					case 51:
+						im = "snow";
+						break;
+					case 52:
+					case 53:
+						im = "rain";
+						break;
+				}
+				break;
+			case 6:
+				im = "snow";
+				break;
+			case 7:
+				im = "fog";
+				break;
+			case 8:
+				switch (id) {
+					case 800:
+						im = ("clear" + timeStr);
+						break;
+					case 801:
+						im = ("partlycloudy" + timeStr);
+						break;
+					case 802:
+						im = "cloudy";
+						break;
+					case 803:
+					case 804:
+						im = "stormclouds";
+						break;
+				}
+				break;
+		}
+		return path + im + ".png";
+	}
 }
