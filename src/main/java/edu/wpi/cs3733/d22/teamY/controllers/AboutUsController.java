0de5e8d39cde7c0efc5e5e1223aaf86616ceb062
TheTrueShadowFox <63rockinjake@gmail.com>
2022-04-22 01:27:56 -0400

    Worked on AboutUsController and PortraitController some more. Still need teammate photos to add to image files to properly test

--- src/main/java/edu/wpi/cs3733/d22/teamY/controllers/AboutUsController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/controllers/AboutUsController.java
@@ -1,6 +1,7 @@
 package edu.wpi.cs3733.d22.teamY.controllers;
 
 import io.github.palexdev.materialfx.controls.MFXButton;
+
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.shape.Rectangle;
@@ -46,39 +47,87 @@ public class AboutUsController {
     stage.close();
   }
 
+  //Linking to PortraitController
+  PortraitController portrait = new PortraitController();
+
+  // Structured array for convenient portrait loading
+  // [String, String, String, String] = [Name, Role, Quote, Image path]
+  String[] connor = {"Connor Horn", "Lead Software", "Quote", "views/images/Connor"};
+  String[] ben = {"Ben Schmitt", "Assistant Lead", "Quote", "views/images/Ben"};
+  String[] emily = {"Emily Gorelik", "Assistant Lead", "Quote", "views/images/Emily"};
+  String[] nathanA = {"Nathan Anderson", "Product Owner", "Quote", "views/images/NathanA"};
+  String[] sky = {"Sky Tang", "Product Manager", "Quote", "views/images/Sky"};
+  String[] john = {"John Carrotta", "Scrum Master", "Quote", "views/images/John"};
+  String[] jake = {"Jake Brady", "Document Analyst", "Quote", "views/images/Jake"};
+  String[] engjell = {"Engjell Ramadani", "Front End", "Quote", "views/images/Engjell"};
+  String[] nicholas = {"Nicholas Heineman", "Front End", "Quote", "views/images/Nicholas"};
+  String[] nathanP = {"Nathan Pollock", "Back End", "Quote", "views/images/NathanP"};
+  String[] ethan = {"Ethan Catania", "Full Stack", "Quote", "views/images/Ethan"};
+
   // Functions to load specific info into the Portrait template
   @FXML
-  private void loadConnor() {
-    // SceneLoading.loadPopup("views/popups/Portrait.fxml", "views/popups/AboutUs.fxml");
+  private void loadConnor() throws IOException {
+    portrait.rewriteInfo(connor);
+    SceneLoading.loadPopup("views/popups/Portrait.fxml", "views/popups/AboutUs.fxml");
   }
 
   @FXML
-  private void loadBen() {}
+  private void loadBen() throws IOException {
+    portrait.rewriteInfo(ben);
+    SceneLoading.loadPopup("views/popups/Portrait.fxml", "views/popups/AboutUs.fxml");
+  }
 
   @FXML
-  private void loadEmily() {}
+  private void loadEmily() throws IOException {
+    portrait.rewriteInfo(emily);
+    SceneLoading.loadPopup("views/popups/Portrait.fxml", "views/popups/AboutUs.fxml");
+  }
 
   @FXML
-  private void loadNathanA() {}
+  private void loadNathanA() throws IOException {
+    portrait.rewriteInfo(nathanA);
+    SceneLoading.loadPopup("views/popups/Portrait.fxml", "views/popups/AboutUs.fxml");
+  }
 
   @FXML
-  private void loadSky() {}
+  private void loadSky() throws IOException {
+    portrait.rewriteInfo(sky);
+    SceneLoading.loadPopup("views/popups/Portrait.fxml", "views/popups/AboutUs.fxml");
+  }
 
   @FXML
-  private void loadJohn() {}
+  private void loadJohn() throws IOException {
+    portrait.rewriteInfo(john);
+    SceneLoading.loadPopup("views/popups/Portrait.fxml", "views/popups/AboutUs.fxml");
+  }
 
   @FXML
-  private void loadJake() {}
+  private void loadJake() throws IOException {
+    portrait.rewriteInfo(jake);
+    SceneLoading.loadPopup("views/popups/Portrait.fxml", "views/popups/AboutUs.fxml");
+  }
 
   @FXML
-  private void loadEngjell() {}
+  private void loadEngjell() throws IOException {
+    portrait.rewriteInfo(engjell);
+    SceneLoading.loadPopup("views/popups/Portrait.fxml", "views/popups/AboutUs.fxml");
+  }
 
   @FXML
-  private void loadNicholas() {}
+  private void loadNicholas() throws IOException {
+    portrait.rewriteInfo(nicholas);
+    SceneLoading.loadPopup("views/popups/Portrait.fxml", "views/popups/AboutUs.fxml");
+  }
 
   @FXML
-  private void loadNathanP() {}
+  private void loadNathanP() throws IOException {
+    portrait.rewriteInfo(nathanP);
+    SceneLoading.loadPopup("views/popups/Portrait.fxml", "views/popups/AboutUs.fxml");
+  }
 
   @FXML
-  private void loadEthan() {}
+  private void loadEthan() throws IOException {
+    portrait.rewriteInfo(ethan);
+    SceneLoading.loadPopup("views/popups/Portrait.fxml", "views/popups/AboutUs.fxml");
+  }
 }
--- src/main/java/edu/wpi/cs3733/d22/teamY/controllers/PortraitController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/controllers/PortraitController.java
@@ -3,9 +3,12 @@ package edu.wpi.cs3733.d22.teamY.controllers;
 import com.jfoenix.controls.JFXTextArea;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class PortraitController {
 
@@ -14,9 +17,18 @@ public class PortraitController {
     @FXML private Label nameText;
     @FXML private Label roleText;
 
-    @FXML private Rectangle imagePlaceholder;
+    @FXML private ImageView portraitImage;
 
     @FXML private JFXTextArea quoteText;
+
+    //Function to put the right text and image stuff in the right places in the portrait template
+    @FXML
+    void rewriteInfo(String[] name) throws FileNotFoundException {
+        nameText.setText(name[0]);
+        roleText.setText(name[1]);
+        quoteText.setText(name[2]);
+        portraitImage = (new ImageView(new Image(new FileInputStream(name[3]))));
+    }
 
     @FXML
     private void backButton() {
