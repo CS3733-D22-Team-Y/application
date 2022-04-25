fef34ca44f441b1386ca558ad2925f4b480a3403
NathanAwpi <nanderson2@wpi.edu>
2022-04-25 15:40:02 -0400

    please no :(

--- src/main/java/edu/wpi/cs3733/d22/teamY/controllers/Scaling.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/controllers/Scaling.java
@@ -46,8 +46,10 @@ public class Scaling {
 
     // Changes the layout of the item to account for the scale around the center.
     // Not perfect, but good enough for now
-    double widthFactor = 2f * 1200f / 1213f;
-    double heightFactor = 2f * 800f / 837f;
+    double widthFactor = 2f; // 1.5f * WINDOW_DEFAULT_WIDTH / 1200f;
+    double heightFactor = 2f; // 1.5f * WINDOW_DEFAULT_HEIGHT / 800f;
+    System.out.println("Width factor: " + widthFactor);
+    System.out.println("Height Factor: " + heightFactor);
     // Set the layout.
     itemToScale
         .layoutXProperty()
