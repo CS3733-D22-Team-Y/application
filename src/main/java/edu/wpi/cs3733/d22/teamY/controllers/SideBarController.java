f5e875199a16c042d5c3f59cbe92e91fb3643d0e
SkyTang1 <101900263+SkyTang1@users.noreply.github.com>
2022-04-24 22:37:00 -0400

    Skysearch update (#157)
    * Added semi functioning search bar
    Issues to fix:
    -does not take into account words in levenshtein distance, so "lab" results in "map" being the closes string instead of "lab results"
    
    - when a page tries to load from the search bar, a null pointer is thrown
    
    * made the search bar fully functional.
    
    Issues fixed:
    -Clicking on the list view or pressing enter will successfully navigate to the desired page
    Issues to resolve:
    -does not take into account words in levenshtein distance, so "lab" results in "map" being the closes string instead of "lab results"
    
    * fix bugs
    
    Co-authored-by: emgore1016 <51887594+emgore1016@users.noreply.github.com>

--- src/main/java/edu/wpi/cs3733/d22/teamY/controllers/SideBarController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/controllers/SideBarController.java
@@ -1,13 +1,19 @@
 package edu.wpi.cs3733.d22.teamY.controllers;
 
+import edu.wpi.cs3733.d22.teamY.utilTemp.SearchUtil;
+import io.github.palexdev.materialfx.controls.legacy.MFXLegacyListView;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
-import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
@@ -16,19 +22,7 @@ public class SideBarController {
   @FXML AnchorPane mainPane;
 
   // Scaling
-  @FXML private VBox bottomSidebarText;
   @FXML private Rectangle sidebarFrame;
-  @FXML private VBox bottomSidebarHiddenButtons;
-  @FXML private VBox bottomSidebarRectangles;
-  // @FXML private VBox topSidebarText;
-  @FXML private Label mapLabel;
-  @FXML private Label servicesLabel;
-  @FXML private Label equipmentLabel;
-  @FXML private Label tasksLabel;
-  @FXML private Label homeLabel;
-  @FXML private Label inboxLabel;
-  @FXML private Label profileLabel;
-  @FXML private Label logoutLabel;
 
   // Hidden rectangles
   @FXML private Rectangle mapHiddenRect;
@@ -39,17 +33,14 @@ public class SideBarController {
   @FXML private Rectangle inboxHiddenRect;
   @FXML private Rectangle profileHiddenRect;
   @FXML private Rectangle logoutHiddenRect;
-  // Hitboxes
-  @FXML private Rectangle mapButtonHitbox;
-  @FXML private Rectangle servicesButtonHitbox;
-  @FXML private Rectangle equipmentButtonHitbox;
-  @FXML private Rectangle tasksButtonHitbox;
-  @FXML private Rectangle homeButtonHitbox;
-  @FXML private Rectangle inboxButtonHitbox;
-  @FXML private Rectangle profileButtonHitbox;
-  @FXML private Rectangle logoutButtonHitbox;
+
+  // List view stuff
+  @FXML private MFXLegacyListView<String> listView;
+  private ObservableList keyWords = FXCollections.observableArrayList();
 
   Scene currScene;
+  ArrayList<String> searchContent = new ArrayList<String>();
+  String[] pages;
 
   @FXML
   void initialize() throws IOException {
@@ -65,98 +56,39 @@ public class SideBarController {
 
     mainPane.setBackground(Background.EMPTY);
     NewSceneLoading.sideBarController = this;
+
+    this.listView.setItems(keyWords);
+
+    pages =
+        new String[] {
+          "Facilities Services",
+          "Floral Services",
+          "Lab Results",
+          "Laundry Services",
+          "Maintenance Services",
+          "Meal Services",
+          "Medical Equipment Services",
+          "Miscellaneous Services",
+          "Security Services",
+          "Specialist Services",
+          "Translator Services",
+          "Map",
+          /*"Inbox",*/
+          "Personal Settings",
+          "Request Menu"
+        };
+
+    searchContent.addAll(List.of(pages));
   }
 
   @FXML
-  void initializeScale(Scene currScene) throws IOException {
-    // currScene = bottomSidebarText.getScene();
-    // Bottom sidebar text
-    /*
-    bottomSidebarText.layoutYProperty().bind(currScene.heightProperty().subtract(200));
-    bottomSidebarHiddenButtons.layoutYProperty().bind(currScene.heightProperty().subtract(200));
-    bottomSidebarRectangles.layoutYProperty().bind(currScene.heightProperty().subtract(200));
-    // Sidebar Rectangle
-    sidebarFrame.scaleYProperty().bind(currScene.heightProperty().subtract(20).divide(780));
-    sidebarFrame.layoutYProperty().bind(sidebarFrame.scaleYProperty().multiply(390).subtract(380));
-    // Top sidebar text
-    // System.out.println(currScene.heightProperty());
-    // Sidebar label adjusting
-    sidebarTopDynamicScale(mapLabel, 150, 50);
-    sidebarTopDynamicScale(servicesLabel, 101, 40.5);
-    sidebarTopDynamicScale(equipmentLabel, 52, 31);
-    sidebarTopDynamicScale(tasksLabel, 3, 21.5);
-    // Top sidebar buttons
-    sidebarBindToHeight(
-        mapHiddenRect,
-        servicesHiddenRect,
-        equipmentHiddenRect,
-        tasksHiddenRect,
-        mapButtonHitbox,
-        servicesButtonHitbox,
-        equipmentButtonHitbox,
-        tasksButtonHitbox);
+  void initializeScale(Scene currScene) throws IOException {}
 
-    sidebarBindToLabel(mapHiddenRect, mapButtonHitbox, mapLabel);
-    sidebarBindToLabel(servicesHiddenRect, servicesButtonHitbox, servicesLabel);
-    sidebarBindToLabel(equipmentHiddenRect, equipmentButtonHitbox, equipmentLabel);
-    sidebarBindToLabel(tasksHiddenRect, tasksButtonHitbox, tasksLabel);
-
-    sidebarBindToLabel(homeHiddenRect, homeButtonHitbox, homeLabel);
-    sidebarBindToLabel(inboxHiddenRect, inboxButtonHitbox, inboxLabel);
-    sidebarBindToLabel(profileHiddenRect, profileButtonHitbox, profileLabel);
-    sidebarBindToLabel(logoutHiddenRect, logoutButtonHitbox, logoutLabel);
-     */
-  }
-
-  private void resizeMainScreen() {
-    /*
-    mainScreenPane.scaleYProperty().bind(currScene.heightProperty().subtract(20).divide(780));
-    mainScreenPane
-        .layoutYProperty()
-        .bind(mainScreenPane.scaleYProperty().multiply(390).subtract(380));
-
-    mainScreenPane.scaleXProperty().bind(currScene.widthProperty().subtract(280).divide(920));
-    mainScreenPane
-        .layoutXProperty()
-        .bind(mainScreenPane.scaleXProperty().multiply(460).subtract(190));
-     */
-  }
-
-  private void sidebarBindToHeight(Shape... shapes) {
-    resizeMainScreen();
-    Scene currScene = shapes[0].getScene();
-    for (Shape currShape : shapes) {
-      currShape.scaleYProperty().bind(currScene.heightProperty().divide(800));
-    }
-  }
-
-  private void sidebarBindToLabel(Rectangle hiddenRect, Rectangle buttonHitbox, Label label) {
-    hiddenRect.layoutYProperty().bind(label.layoutYProperty());
-    buttonHitbox.layoutYProperty().bind(label.layoutYProperty());
-  }
-
-  private void sidebarTopDynamicScale(
-      Label label, double multiplyProperty, double subtractProperty) {
-    Scene currScene = label.getScene();
-    label
-        .layoutYProperty()
-        .bind(
-            currScene
-                .heightProperty()
-                .divide(4)
-                .subtract(sidebarFrame.scaleYProperty().multiply(multiplyProperty))
-                .subtract(subtractProperty));
-  }
+  private void resizeMainScreen() {}
 
   @FXML
   void killApplication() throws IOException {
     SceneLoading.loadPopup("views/popups/ConfirmClose.fxml", "views/SideBar.fxml");
-  }
-
-  @FXML
-  void dbSwitcherToggle() {
-    // boolean state = dbToggle.isSelected();
-    // DBUtils.switchDBType(state);
   }
 
   @FXML
@@ -254,9 +186,6 @@ public class SideBarController {
 
   @FXML
   void loadMap() throws IOException {
-    // SceneUtil.hideAllPanes(mainScreenPane.getChildren());
-    // mainScreenPane.getChildren().get(MAP_LIST_LOCATION).setVisible(true);
-    // SceneLoading.loadPopup("views/popups/ReqAbort.fxml", "views/RequestMenu.fxml");
     NewSceneLoading.loadScene("views/Map.fxml");
     setButtonSelected(mapHiddenRect);
   }
@@ -268,33 +197,19 @@ public class SideBarController {
   }
 
   @FXML
-  void loadViewServiceRequests() throws IOException {
-    // Reset to the main request screen
-    /*
-    AnchorPane bgPane =
-        (AnchorPane) mainScreenPane.getChildren().get(SERVICES_LIST_LOCATION).lookup("#bgPane");
-    SceneUtil.hideAllPanes(bgPane.getChildren());
-    bgPane.getChildren().get(RequestMenuController.MAIN_PAGE_INDEX).setVisible(true);
-    // Main code
-    SceneUtil.hideAllPanes(mainScreenPane.getChildren());
-    mainScreenPane.getChildren().get(SERVICES_LIST_LOCATION).setVisible(true);
-     */
+  void loadViewServiceRequests() {
     setButtonSelected(servicesHiddenRect);
     NewSceneLoading.loadScene("views/RequestMenu.fxml");
   }
 
   @FXML
   void loadEquipment() {
-    // SceneUtil.hideAllPanes(mainScreenPane.getChildren());
-    // mainScreenPane.getChildren().get(EQUIPMENT_LIST_LOCATION).setVisible(true);
     NewSceneLoading.loadScene("views/MedEquipTable.fxml");
     setButtonSelected(equipmentHiddenRect);
   }
 
   @FXML
   void loadTasks() {
-    // SceneUtil.hideAllPanes(mainScreenPane.getChildren());
-    // mainScreenPane.getChildren().get(TASKS_LIST_LOCATION).setVisible(true);
     setButtonSelected(tasksHiddenRect);
     NewSceneLoading.loadScene("views/ActiveServiceRequest.fxml");
   }
@@ -347,85 +262,47 @@ public class SideBarController {
    */
   @FXML
   public void doSearch() throws IOException {
-    String entry = searchBar.getText();
+    String goTo = getDirectory(listView.getSelectionModel().getSelectedItem());
+    NewSceneLoading.reloadScene(goTo);
+    NewSceneLoading.loadScene(goTo);
+    this.searchBar.setText("");
+    this.listView.setVisible(false);
+  }
 
-    // Valid pages: !!! NEEDS TO UPDATE EVERYTIME NEW PAGE IS ADDED !!!
-    String[] pages = {
-      "floral",
-      "lab",
-      "laundry",
-      "meal",
-      "medical",
-      "security",
-      "map",
-      "settings",
-      "menu",
-      "request",
-      "profile",
-      "translator",
-      "tasks"
-    };
+  @FXML
+  public void getSearchItems() {
 
-    // The following lines of code filters and processes search request
-    entry = entry.toLowerCase();
-    entry.replaceAll(" ", "");
-    entry.replaceAll("active", "table");
-    entry.replaceAll("current", "table");
-    entry.replaceAll("equipment", "medical");
+    String entry = this.searchBar.getText();
+    entry = entry.toLowerCase(Locale.ROOT);
 
-    // Will see if this request exists
-    String isValid = getPage(entry, pages);
-    if (isValid == "ERROR") {
-      // TODO: Levenshtein distance computations
+    if (entry.length() != 0 || entry != null) {
+      populateListView(entry, pages);
     } else {
-      entry = isValid;
+      this.listView.setVisible(false);
     }
   }
 
-  /** Helper method for doSearch Will determine if a valid page exists */
-  public String getPage(String entry, String[] pages) throws IOException {
-    // for (int i = 0; i < pages.length; i++) {
-    // if (entry.contains(pages[i])) {
-    // i = pages.length;
-    if (entry.contains("medical")) {
-      loadViewServiceRequests();
-      SceneUtil.requests.loadMedEquipReq();
-    } else if (entry.contains("laundry")) {
-      loadViewServiceRequests();
-      SceneUtil.requests.loadLaundryReq();
-    } else if (entry.contains("meal")) {
-      loadViewServiceRequests();
-      SceneUtil.requests.loadMealReq();
-    } else if (entry.contains("security")) {
-      loadViewServiceRequests();
-      SceneUtil.requests.loadSecurityReq();
-    } else if (entry.contains("floral")) {
-      loadViewServiceRequests();
-      SceneUtil.requests.loadFloralReq();
-    } else if (entry.contains("translator")) {
-      loadViewServiceRequests();
-      SceneUtil.requests.loadTranslatorReq();
-    } else if (entry.contains("settings") || entry.contains("profile")) {
-      loadProfile();
-    } else if (entry.contains("security")) {
-      loadViewServiceRequests();
-      SceneUtil.requests.loadSecurityReq();
-    } else if (entry.contains("map")) {
-      loadMap();
-    } else if (entry.contains("request")) {
-      loadViewServiceRequests();
-      SceneUtil.requests.showMainPane();
-    } else if (entry.contains("equipment")) {
-      loadEquipment();
-    } else if (entry.contains("tasks")) {
-      loadTasks();
+  public void populateListView(String entry, String[] pagesArray) {
+    System.out.println("----------------------------------------" + entry);
+    searchContent.sort(
+        Comparator.comparingInt(o -> SearchUtil.getMatchScore(o.toLowerCase(), entry)));
+    this.keyWords.clear();
+    this.keyWords.addAll(searchContent.get(0), searchContent.get(1), searchContent.get(2));
+    this.listView.setVisible(true);
+    this.listView.getSelectionModel().selectFirst();
+  }
+
+  public String getDirectory(String page) {
+    page = page.replaceAll("Services", "Request");
+    page = page.replaceAll("Results", "Result");
+    page = page.replaceAll(" ", "");
+    page = page.replaceAll("ellaneous", "");
+
+    if (page.toLowerCase().contains("map") || page.toLowerCase().contains("menu")) {
+      page = "views/" + page + ".fxml";
+    } else {
+      page = "views/requestTypes/" + page + ".fxml";
     }
-    // } else {
-    //  entry = entry.substring(0).toUpperCase();
-    //  entry = entry + "Request.fxml";
-    // }
-    // entry = "views/" + entry;
-    // }
-    return "";
+    return page;
   }
 }
