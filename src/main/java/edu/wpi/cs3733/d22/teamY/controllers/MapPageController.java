a6d0fc7413f88349b376978f483e0da09c7cb442
NathanAwpi <nanderson2@wpi.edu>
2022-04-25 13:03:24 -0400

    made map less not pretty

--- src/main/java/edu/wpi/cs3733/d22/teamY/controllers/MapPageController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/controllers/MapPageController.java
@@ -670,8 +670,8 @@ public class MapPageController<T extends Requestable> {
     locationPin.setImage(new Image(App.class.getResource("views/images/icons/pin.png").toString()));
     locationPin.setFitHeight(25);
     locationPin.setFitWidth(25);
-    locationPin.setLayoutX(995);
-    locationPin.setLayoutY(10);
+    locationPin.setLayoutX(985);
+    locationPin.setLayoutY(20);
     mainPane.getChildren().add(locationPin);
     locationPin.setTranslateY(0);
     locationPin.setTranslateX(0);
@@ -687,8 +687,8 @@ public class MapPageController<T extends Requestable> {
 
     locationPin.setOnMouseReleased(
         e -> {
-          locationPin.setLayoutX(995);
-          locationPin.setLayoutY(10);
+          locationPin.setLayoutX(985);
+          locationPin.setLayoutY(20);
           System.out.println(currentFloor);
           Robot bot = null;
           try {
