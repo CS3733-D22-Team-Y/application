9ee673dea31c0af5339b68acd5c648aa35f679bc
Ethan Catania <ercatania@wpi.edu>
2022-04-28 23:16:30 -0400

    comments

--- src/main/java/edu/wpi/cs3733/d22/teamY/controllers/RequestMenuController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/controllers/RequestMenuController.java
@@ -93,7 +93,6 @@ public class RequestMenuController implements IController {
 
   @FXML
   void loadSecurityReq() {
-    // loadRequestScreen(SECURITY_RESULTS_INDEX);
     NewSceneLoading.loadScene("views/requestTypes/SecurityRequest.fxml");
   }
 
